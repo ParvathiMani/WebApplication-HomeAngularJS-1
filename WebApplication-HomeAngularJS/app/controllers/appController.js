0f22f17744b1d0b42743700df5d982e8ba7e3ad4
ParvathiManimuthu <parvathi.manimuthu@outlook.com>
2017-04-12 05:27:35 +0530

    Home Update - First-Level Code Commit

---
+++ WebApplication-HomeAngularJS/app/controllers/appController.js
@@ -0,0 +1,11 @@
+﻿'use strict';
+//app.controller('appController', ['$scope', function ($scope) {
+//    $scope.starRating = 3;
+//}]);
+
+
+var app = angular.module("app", []);
+app.controller("appController", function ($scope) {
+    $scope.firstName = "John";
+    $scope.lastName = "Doe";
+});
